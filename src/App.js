19caf09ba6c8b1f445e65a7cddb77c0d0d2a8400
Satyen Verma <sverma44@asu.edu>
2020-07-27 17:12:09 -0700

    Added invocation of BooksAPI's getAll method to get all books

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -10,10 +10,18 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    books: []
+  }
+
+  componentDidMount() {
+    //Invoke BooksAPI's getAll() method to get all the books
+    BooksAPI.getAll().then(books => this.setState({ books }));
   }
 
   render() {
+    const { books } = this.state;
+    
     return (
       <div className="app">
         {this.state.showSearchPage ? (
